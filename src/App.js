8e02077a737422a67cb7b6654891856636479cb0
Agustinn-1002 <agupasten@gmail.com>
2021-03-01 17:16:31 -0300

    'end-website'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Section from './components/Section';
+import {GlobalStyle} from './styled/Global'
 
 function App() {
+  const [toogleIcon, setToogleIcon] = useState(false)
+
+  const toogleNavIcon = () => setToogleIcon(!toogleIcon)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar toogleIcon={toogleIcon} toogleNavIcon={toogleNavIcon}></Navbar>
+      <Header toogleIcon={toogleIcon} toogleNavIcon={toogleNavIcon}></Header>
+      <Section toogleIcon={toogleIcon} toogleNavIcon={toogleNavIcon}></Section>
+      <Footer toogleIcon={toogleIcon} toogleNavIcon={toogleNavIcon}></Footer>
+    </>
   );
 }
 
